8f067236660d86ff2c583f6c5f4195bd7f784b9c
GAUTHAM <GAUTHAM@DESKTOP-0I12IA8>
2019-12-08 17:03:52 +0530

    First Comit
    Uploading All files

---
+++ test/src/main/java/com/atch/test/App.java
@@ -0,0 +1,38 @@
+package com.atch.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Configuration ssr = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class);  
+      //  Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
+      
+    SessionFactory factory = ssr.buildSessionFactory();  
+    Session session = factory.openSession();  
+    Transaction t = session.beginTransaction();  
+      
+     Employee e1=new Employee();    
+        e1.setId(4);    
+        e1.setFirstName("rishab");    
+        e1.setLastName("pant");    
+     
+   session.save(e1);  
+   t.commit();  
+   System.out.println("successfully saved");    
+    factory.close();  
+    session.close();     
+    }
+}
